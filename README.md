146f661e4a23d6dac00725d4443907be36fa0114
Shinei Arakawa <arakawashinei1115@gmail.com>
2025-03-12 16:01:43 +0200

    Update

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # research-utilities
 
-**Just fot me**
+This repository contains a collection of utility scripts and functions commonly used in my research.
+
+## Features
+
+- Common data preprocessing utilities
+- Visualization tools
+- Machine learning helper functions
+- Computational tools for scientific research
+
+## Get started with Python
+
+```bash
+pip install https://github.com/ShineiArakawa/research-utilities.git
+```
+
+```bash
+uv add https://github.com/ShineiArakawa/research-utilities.git
+```
---
+++ src/research_utilities/multi_processing.py
@@ -0,0 +1,70 @@
+import multiprocessing as mp
+import os
+import typing
+
+import tqdm.auto
+
+
+def launch_multi_process(
+    func: typing.Callable,
+    args: list,
+    n_processes: int | None = None,
+    verbose: bool = True,
+    desc: str = 'Processing ... ',
+    context: str = 'spawn',
+    is_unordered: bool = False
+) -> list:
+    """Launch multiple processes to execute a function.
+
+    Parameters
+    ----------
+    func : typing.Callable
+        The function to be executed
+    args : list
+        The arguments to be passed to the function
+    n_processes : int | None, optional
+        The number of processes to be launched, by default None (use all available CPUs)
+    verbose : bool, optional
+        Whether to show the progress bar, by default True
+    desc : str, optional
+        The description of the progress bar, by default 'Processing ... '
+    context : str, optional
+        The context of the multiprocessing pool, by default 'spawn'
+    is_unordered : bool, optional
+        Whether to use unordered processing, by default False
+        If True, the results will be returned in the order they are completed.
+
+    Returns
+    -------
+    list
+        The results of the function
+
+    Usage
+    -----
+    ```python
+    def func(arg):
+        return arg[0] + arg[1]
+
+    args = [(1, 2), (3, 4), (5, 6)]
+    results = launch_multi_process(func, args, n_processes=2)
+
+    print(results)
+    # Output: [3, 7, 11]
+    ```
+
+    """
+    n_processes = n_processes or os.cpu_count()
+
+    with mp.get_context(context).Pool(processes=n_processes) as pool:
+        map_fn = pool.imap_unordered if is_unordered else pool.imap
+
+        results = list(
+            tqdm.auto.tqdm(
+                map_fn(func, args),
+                total=len(args),
+                desc=desc,
+                disable=not verbose
+            )
+        )
+
+    return results
